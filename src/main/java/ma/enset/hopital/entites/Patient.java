7b1f884689632127b831e9b61efe5b609a1e239e
Ayoub <Fanaoui.25@gmail.com>
2025-03-17 11:17:49 +0100

    Edit Patient class: Update attributes and constructors

--- src/main/java/ma/enset/hopital/entites/Patient.java
+++ src/main/java/ma/enset/hopital/entites/Patient.java
@@ -1,9 +1,22 @@
 package ma.enset.hopital.entites;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.autoconfigure.amqp.RabbitConnectionDetails;
 
 import java.util.Date;
+
+
+@Entity
+@Data @AllArgsConstructor @NoArgsConstructor @Builder
 public class Patient {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nom;
     private Date dateNaissance;

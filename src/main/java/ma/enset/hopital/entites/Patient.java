c1c8e4a715559a239d3d7df3a78cac61d3a5dcba
Ayoub <Fanaoui.25@gmail.com>
2025-03-17 10:44:26 +0100

    Initialize the project and create the Patient class.

---
+++ src/main/java/ma/enset/hopital/entites/Patient.java
@@ -0,0 +1,13 @@
+package ma.enset.hopital.entites;
+
+import org.springframework.boot.autoconfigure.amqp.RabbitConnectionDetails;
+
+import java.util.Date;
+
+public class Patient {
+    private Long id;
+    private String nom;
+    private Date dateNaissance;
+    private boolean malade;
+    private int score;
+}

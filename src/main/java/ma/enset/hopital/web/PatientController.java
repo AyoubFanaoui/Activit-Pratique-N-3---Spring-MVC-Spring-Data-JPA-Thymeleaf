0d1492b82eb9279a3b84d054340b01482f5ef616
Ayoub <Fanaoui.25@gmail.com>
2025-03-24 12:16:13 +0100

    Create an HTML file for the patient and update the Patient entity

--- src/main/java/ma/enset/hopital/web/PatientController.java
+++ src/main/java/ma/enset/hopital/web/PatientController.java
@@ -1,16 +1,24 @@
 package ma.enset.hopital.web;
 
 import lombok.AllArgsConstructor;
+import ma.enset.hopital.entites.Patient;
 import ma.enset.hopital.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 @AllArgsConstructor
 public class PatientController {
+    @Autowired
     private PatientRepository patientRepository;
     @GetMapping("/index")
-    public String index(){
+    public String index(Model model) {
+        List<Patient> patientList=patientRepository.findAll();
+        model.addAttribute("listpatients", patientList);
         return "patients";
     }
 }
